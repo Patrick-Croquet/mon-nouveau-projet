c7192dc1829e7c29ea28f458713d50a5acf6c5ad
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2021-03-09 16:35:30 +0100

    Initial commit

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\User;
+use App\Entity\Genre;
 use App\Entity\Auteur;
 use App\Entity\Editeur;
-use App\Entity\Fournisseur;
-use App\Entity\Genre;
 use App\Entity\Produit;
+use App\Entity\Fournisseur;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -34,6 +35,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linktoDashboard('Accueil', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-tags', User::class);
         yield MenuItem::section('Mes BD');
             yield MenuItem::linkToCrud('Produits', 'fas fa-tags', Produit::class);
             yield MenuItem::linkToCrud('Auteurs', 'fas fa-tags', Auteur::class);
